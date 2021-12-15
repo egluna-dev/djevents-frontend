e493cb85109db508748c2f8a4ff3e6ec692cadb5
egluna-dev <egluna@live.ca>
2021-12-15 23:26:18 +1100

    Layout, Header, Footer

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,14 @@
+import Link from 'next/link';
+import Layout from '../components/Layout';
+
 export default function AboutPage() {
     return (
-        <div>
+        <Layout title='About DJ Events'>
             <h1>About Page</h1>
             <p>This is an app to find the latest dj and music events in your city.</p>
-        </div>
+            <p>Version: 1.0.0</p>
+            <Link href='/'>Home</Link>
+        </Layout>
     )
 }
 
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,9 @@
+import Layout from '../../components/Layout';
+
 export default function EventPage() {
     return (
-        <div>
-            
-        </div>
+        <Layout>
+            <h1>My Event: Yes Party</h1>
+        </Layout>
     )
 }
