37498d0d3328db7894372ac277a80715b5677f3f
egluna-dev <egluna@live.ca>
2021-12-15 21:57:34 +1100

    pages and routing

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>This is an app to find the latest dj and music events in your city.</p>
+        </div>
+    )
+}
+
