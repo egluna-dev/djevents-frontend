e518bcb34a75233b3b3943c9752dc04e8c6729dd
egluna-dev <egluna@live.ca>
2021-12-17 17:48:25 +1100

    Events index page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,28 @@
-import Layout from '../../components/Layout';
+// import Link from 'next/link';
+import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
+import EventItem from '@/components/EventItem';
 
-export default function EventsPage() {
-    return (
-        <Layout>
-            <h1>My Events</h1>
-        </Layout>
-    )
+export default function EventsPage({ events }) {
+  return (
+    <Layout>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No Events to Show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+    </Layout>
+  )
 }
+
+export async function getStaticProps() {
+  const resp = await fetch(`${API_URL}/api/events`);
+  const events = await resp.json();
+
+  return {
+    props: { events },
+    revalidate: 1
+  }
+}
+
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,34 @@
+// import Link from 'next/link';
+import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
+import EventItem from '@/components/EventItem';
 import Link from 'next/link';
-import Layout from '../components/Layout';
 
-export default function Home() {
+export default function HomePage({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
-      <Link href='/about'>About page</Link>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No Events to Show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href='/events'>
+          
+        </Link>
+      )}
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const resp = await fetch(`${API_URL}/api/events`);
+  const events = await resp.json();
+
+  return {
+    props: { events: events.slice(0,3) },
+    revalidate: 1
+  }
 }
